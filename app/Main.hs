40e05249c67311203efecaf315e3388912ea311c
Luke Winikates <luke.winikates@gmail.com>
2017-05-24 21:19:45 -0700

    made it loop, so that you can see multiple gifs from the same search

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@
 
 module Main (main) where
 
+-- todo: extract it all into something in Lib, so that main can be simpler?
 import Text.Read (readMaybe)
 import Lib
 import System.Process as P (callCommand)
@@ -30,31 +31,38 @@ tabularize items =
       (B.text "Giphy Slug")
       (map (B.text . slug) items))
 
-randomNumber :: StdGen -> Int -> Int
+randomNumber :: StdGen -> Int -> (Int, StdGen)
 randomNumber gen len =
-    let (number, _) = randomR (1, len) gen in number
+    randomR (1, len) gen
 
 orElse :: Maybe a -> a -> a
 orElse (Just value) _ = value
 orElse Nothing altValue = altValue
 
-pickAGiphy :: [GiphyItem] -> IO GiphyItem
-pickAGiphy giphies = do
-  gen <- getStdGen
+-- todo: the flip on the tuple construction is maybe too clever
+-- the let line is maybe too long
+-- I wonder if pickAGiphy could be a little more linear, e.g. with the Maybe function
+pickAGiphy :: [GiphyItem] -> StdGen -> IO (GiphyItem, StdGen)
+pickAGiphy giphies gen = do
   userSelectedNumber <- fmap charToNumber getLine
-  let randomSelection = randomNumber gen (length giphies)
-      n = userSelectedNumber `orElse` randomSelection
-    in return (nth n giphies)
+  let (n, g) = fmap (flip (,) gen) userSelectedNumber `orElse` randomNumber gen (length giphies)
+    in return (nth n giphies, g)
+
+keepPicking :: [GiphyItem] -> StdGen -> IO ()
+keepPicking giphies gen =
+   printBox (tabularize giphies) >>
+   putStrLn "which one would you like to open? (type a number and press <enter>, or just press enter to get a random gif)" >>
+   pickAGiphy giphies gen >>= \(giphy, newGen) ->
+   P.callCommand ("open " ++ embedUrl giphy) >>
+   keepPicking giphies newGen
 
 instance MonadHttp IO where
   handleHttpException = throwIO
 
+-- todo: only print the table once (or offer an option to print it again)
 main :: IO ()
 main =
    putStrLn "enter a search term and press <enter>" >>
    getLine >>= giphySearch >>= \(Lib.GiphyList giphies) ->
-   printBox (tabularize giphies) >>
-   putStrLn "which one would you like to open? (type a number and press <enter>, or just press enter to get a random gif)" >>
-   pickAGiphy giphies >>= \giphy ->
-   P.callCommand $ "open " ++ embedUrl giphy
+   keepPicking giphies =<< getStdGen
 
